3518becc61407b2e8795880e43bb49775bd9ea47
Dayan Freitas <dayan.freitas@catolicasc.org.br>
2021-11-02 20:55:53 -0300

    setup de test

---
+++ src/blackjack_test.go
@@ -0,0 +1,21 @@
+package blackjack
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPaymentOddsThreeForTwo(t *testing.T) {
+	odds := float64(3.0 / 2.0)
+	betValue := 100.0
+
+	assert.Equal(t, 150.0, betValue*odds, "Pagamento do cassino 3 para 2")
+}
+
+func TestPaymentOddsSixForFive(t *testing.T) {
+	odds := (6.0 / 5.0)
+	betValue := 100.0
+
+	assert.Equal(t, 120.0, betValue*odds, "Pagamento do cassino 6 para 5")
+}
