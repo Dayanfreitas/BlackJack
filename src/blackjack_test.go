8b1edb340c71ab0c8e4652a783bee92a537d39f0
Dayan Freitas <dayan.freitas@catolicasc.org.br>
2021-11-02 21:09:25 -0300

    TDD

--- src/blackjack_test.go
+++ src/blackjack_test.go
@@ -10,12 +10,12 @@ func TestPaymentOddsThreeForTwo(t *testing.T) {
 	odds := float64(3.0 / 2.0)
 	betValue := 100.0
 
-	assert.Equal(t, 150.0, betValue*odds, "Pagamento do cassino 3 para 2")
+	assert.Equal(t, 150.0, PaymentOdds(odds, betValue), "Pagamento do cassino 3 para 2")
 }
 
 func TestPaymentOddsSixForFive(t *testing.T) {
 	odds := (6.0 / 5.0)
 	betValue := 100.0
 
-	assert.Equal(t, 120.0, betValue*odds, "Pagamento do cassino 6 para 5")
+	assert.Equal(t, 120.0, PaymentOdds(odds, betValue), "Pagamento do cassino 6 para 5")
 }
